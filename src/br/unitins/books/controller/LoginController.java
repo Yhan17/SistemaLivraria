b78f3777e4ce6107ea464297ef9e65d0ed62cdf6
flamolino <rafaelskate7@gmail.com>
2020-02-27 08:56:32 -0300

    Projeto Criado

---
+++ src/br/unitins/books/controller/LoginController.java
@@ -0,0 +1,14 @@
+package br.unitins.books.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+@Named
+@RequestScoped
+public class LoginController implements Serializable {
+
+	private static final long serialVersionUID = -6983839857205389929L;
+
+
+}
