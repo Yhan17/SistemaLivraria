ad7ea3255ae7025bccccde3e96b5dda728b2fe8d
Yhan17 <shikamaru.nara897@gmail.com>
2020-02-27 11:31:49 -0300

    Modificação do cadastro de Usuarios

--- src/br/unitins/books/controller/UsuarioController.java
+++ src/br/unitins/books/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package br.unitins.books.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -12,13 +14,31 @@ import br.unitins.books.model.Usuario;
 public class UsuarioController implements Serializable {
 
 	private static final long serialVersionUID = -563227102506849534L;
-	
+
 	private Usuario usuario;
+	private List<Usuario> listaUsuario ;
 
 	public void incluir() {
+		listaUsuario.add(getUsuario());
+		limpar();
+	}
+
+	public void limpar() {
+		usuario = null;
+	}
+
+	public void alterar() {
 		System.out.println(getUsuario().getNome());
 	}
+
+	public void remover() {
+		listaUsuario.remove(getUsuario());
 	
+	}
+	public void editar(Usuario usu) {
+		setUsuario(usu);
+	}
+
 	public Usuario getUsuario() {
 		if (usuario == null)
 			usuario = new Usuario();
@@ -28,6 +48,11 @@ public class UsuarioController implements Serializable {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
 
+	public List<Usuario> getListaUsuario() {
+		if(listaUsuario == null) {
+			listaUsuario = new ArrayList<Usuario>();
+		}
+		return listaUsuario;
+	}
 }
