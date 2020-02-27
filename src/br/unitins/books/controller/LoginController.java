3c0decaa6ed0292eda55296a84504004c2009038
flamolino <rafaelskate7@gmail.com>
2020-02-27 10:09:38 -0300

    Adicao ao usuario

--- src/br/unitins/books/controller/LoginController.java
+++ src/br/unitins/books/controller/LoginController.java
@@ -3,12 +3,29 @@ package br.unitins.books.controller;
 import java.io.Serializable;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-@Named
+@Named("loginController")
 @RequestScoped
 public class LoginController implements Serializable {
 
 	private static final long serialVersionUID = -6983839857205389929L;
+	
+	private String login;
+	private String senha;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 
 }
